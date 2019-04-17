f56eca53286bf82a3ff2926a00e3a9fd27aa0575
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2019-04-17 09:46:43 -0700

    Allow specifying a service connection for the Pulumi task (#1)
    * WIP. Enable service endpoint input instead of using env vars for specifying the ARM creds.
    
    * Add icon. Add missing file for serviceEndpoint.
    
    * Use process.env to feed the PATH to the exec options.
    
    * Require the service principal client ID and the subscription ID.

--- buildAndReleaseTask/index.ts
+++ buildAndReleaseTask/index.ts
@@ -1,10 +1,12 @@
 // Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 import * as tl from "azure-pipelines-task-lib/task";
+import * as tr from "azure-pipelines-task-lib/toolrunner";
 import * as toolLib from "azure-pipelines-tool-lib/tool";
 import * as path from "path";
 import { installUsingCurl } from "./installers/curl";
 import { installUsingPowerShell } from "./installers/powershell";
+import { getServiceEndpoint } from "./serviceEndpoint";
 
 const pulumiVersion = "0.17.5";
 
@@ -12,6 +14,12 @@ async function run() {
     tl.setResourcePath(path.join(__dirname, "task.json"));
 
     tl.debug(tl.loc("Debug_Starting"));
+
+    const connectedServiceName = tl.getInput("azureSubscription", true);
+
+    tl.debug(tl.loc("Debug_ServiceEndpointName", connectedServiceName));
+    const serviceEndpoint = getServiceEndpoint(connectedServiceName);
+    tl.debug(`Service endpoint retrieved with client ID ${serviceEndpoint.clientId}`);
 
     let toolPath = toolLib.findLocalTool("pulumi", pulumiVersion);
     if (!toolPath) {
@@ -55,23 +63,40 @@ async function run() {
         }
 
         // Get the working directory where the Pulumi commands must be run.
+        const pathEnv = process.env["PATH"];
+        tl.debug(`Executing Pulumi commands with PATH ${ pathEnv }`);
         const pulCwd = tl.getInput("cwd") || ".";
+        const pulExecOptions = {
+            cwd: pulCwd,
+            env: {
+                ARM_CLIENT_ID: serviceEndpoint.clientId,
+                ARM_CLIENT_SECRET: serviceEndpoint.servicePrincipalKey,
+                ARM_SUBSCRIPTION_ID: serviceEndpoint.subscriptionId,
+                ARM_TENANT_ID: serviceEndpoint.tenantId,
+                PATH: pathEnv,
+            },
+            // Set defaults.
+            errStream: process.stderr,
+            failOnStdErr: false,
+            ignoreReturnCode: true,
+            outStream: process.stdout,
+            silent: false,
+            windowsVerbatimArguments: false,
+        } as tr.IExecOptions;
         // Select the stack.
         const pulStack = tl.getInput("stack", true);
-        await tl.exec(toolPath, ["--cwd", pulCwd, "stack", "select", pulStack]);
+        await tl.exec(toolPath, ["stack", "select", pulStack], pulExecOptions);
 
         // Get the command, and the args the user wants to pass to the Pulumi CLI.
         const pulCommand = tl.getInput("command");
         const pulCommandRunner =
             await tl.tool(toolPath)
-                    .arg("--cwd")
-                    .arg(pulCwd)
                     .arg(pulCommand);
         const pulArgs = tl.getDelimitedInput("args", " ");
         pulArgs.forEach((arg: string) => {
             pulCommandRunner.arg(arg);
         });
-        exitCode = await pulCommandRunner.exec();
+        exitCode = await pulCommandRunner.exec(pulExecOptions);
         if (exitCode !== 0) {
             tl.setResult(tl.TaskResult.Failed,
                 tl.loc("PulumiCommandFailed", exitCode, `${ pulCommand } ${ pulArgs.join(" ") }`));
---
+++ buildAndReleaseTask/serviceEndpoint.ts
@@ -0,0 +1,21 @@
+// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+
+import * as tl from "azure-pipelines-task-lib/task";
+
+export interface IServiceEndpoint {
+    subscriptionId: string;
+    servicePrincipalKey: string;
+    tenantId: string;
+    clientId: string;
+}
+
+export function getServiceEndpoint(connectedServiceName: string): IServiceEndpoint {
+    const endpoint = {
+        clientId: tl.getEndpointAuthorizationParameter(connectedServiceName, "serviceprincipalid", false),
+        servicePrincipalKey: tl.getEndpointAuthorizationParameter(connectedServiceName, "serviceprincipalkey", false),
+        subscriptionId: tl.getEndpointDataParameter(connectedServiceName, "subscriptionid", false),
+        tenantId: tl.getEndpointAuthorizationParameter(connectedServiceName, "tenantid", false),
+    } as IServiceEndpoint;
+
+    return endpoint;
+}
