e544d38ebd528f21e5efbb3db31df250de093ea7
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2019-04-29 13:47:24 -0700

    Remove unused test case.

--- buildAndReleaseTask/tests/_suite.ts
+++ buildAndReleaseTask/tests/_suite.ts
@@ -25,21 +25,4 @@ describe("Pulumi task tests", () => {
         assert.equal(tr.stdout.indexOf("fake pulumi preview") >= 0, true, "should execute `pulumi preview` command");
         done();
     });
-
-    // it("it should fail if tool returns 1", (done: MochaDone) => {
-    //     this.timeout(1000);
-
-    //     let tp = path.join(__dirname, "failure.js");
-    //     let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
-
-    //     tr.run();
-    //     console.log(tr.succeeded);
-    //     assert.equal(tr.succeeded, false, "should have failed");
-    //     assert.equal(tr.warningIssues, 0, "should have no warnings");
-    //     assert.equal(tr.errorIssues.length, 1, "should have 1 error issue");
-    //     assert.equal(tr.errorIssues[0], "Bad input was given", "error issue output");
-    //     assert.equal(tr.stdout.indexOf("Hello bad"), -1, "Should not display Hello bad");
-
-    //     done();
-    // });
 });
