608380638067a3d559ab01e13845fdcaba09db99
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2019-04-30 18:33:34 -0700

    Move the pulumi-specific code to its own file.

--- buildAndReleaseTask/index.ts
+++ buildAndReleaseTask/index.ts
@@ -1,12 +1,12 @@
 // Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 import * as tl from "azure-pipelines-task-lib/task";
-import * as tr from "azure-pipelines-task-lib/toolrunner";
 import * as toolLib from "azure-pipelines-tool-lib/tool";
 import * as path from "path";
 
 import { installUsingCurl } from "./installers/curl";
 import { installUsingPowerShell } from "./installers/powershell";
+import { runPulumi } from "./pulumi";
 import { getServiceEndpoint } from "./serviceEndpoint";
 import { getLatestPulumiVersion } from "./version";
 
@@ -25,7 +25,7 @@ async function run() {
     const serviceEndpoint = getServiceEndpoint(connectedServiceName);
     tl.debug(`Service endpoint retrieved with client ID ${serviceEndpoint.clientId}`);
 
-    let toolPath = toolLib.findLocalTool("pulumi", pulumiVersion);
+    const toolPath = toolLib.findLocalTool("pulumi", pulumiVersion);
     if (!toolPath) {
         tl.debug(tl.loc("Debug_NotFoundInCache"));
         try {
@@ -46,73 +46,8 @@ async function run() {
         toolLib.prependPath(path.join(toolPath, "bin"));
     }
 
-    try {
-        tl.debug(tl.loc("Debug_Installed"));
-        // Print the version.
-        toolPath = tl.which("pulumi");
-        tl.debug(tl.loc("Debug_PrintToolPath", toolPath));
-        if (!toolPath) {
-            tl.setResult(tl.TaskResult.Failed, tl.loc("PulumiNotFound"));
-            return;
-        }
-        tl.debug(tl.loc("Debug_PrintingVersion"));
-        await tl.exec(toolPath, "version");
-
-        // Login and then run the command.
-        tl.debug(tl.loc("Debug_Login"));
-        let exitCode = await tl.exec(toolPath, "login");
-        if (exitCode !== 0) {
-            tl.setResult(tl.TaskResult.Failed, tl.loc("PulumiLoginFailed"));
-            return;
-        }
-
-        // Get the working directory where the Pulumi commands must be run.
-        const pathEnv = process.env["PATH"];
-        tl.debug(`Executing Pulumi commands with PATH ${ pathEnv }`);
-        const pulCwd = tl.getInput("cwd") || ".";
-        const pulExecOptions = {
-            cwd: pulCwd,
-            env: {
-                ARM_CLIENT_ID: serviceEndpoint.clientId,
-                ARM_CLIENT_SECRET: serviceEndpoint.servicePrincipalKey,
-                ARM_SUBSCRIPTION_ID: serviceEndpoint.subscriptionId,
-                ARM_TENANT_ID: serviceEndpoint.tenantId,
-                PATH: pathEnv,
-            },
-            // Set defaults.
-            errStream: process.stderr,
-            failOnStdErr: false,
-            ignoreReturnCode: true,
-            outStream: process.stdout,
-            silent: false,
-            windowsVerbatimArguments: false,
-        } as tr.IExecOptions;
-        // Select the stack.
-        const pulStack = tl.getInput("stack", true);
-        exitCode = await tl.exec(toolPath, ["stack", "select", pulStack], pulExecOptions);
-        if (exitCode !== 0) {
-            tl.setResult(tl.TaskResult.Failed, tl.loc("PulumiStackSelectFailed", pulStack));
-            return;
-        }
-
-        // Get the command, and the args the user wants to pass to the Pulumi CLI.
-        const pulCommand = tl.getInput("command", true);
-        const pulCommandRunner =
-            await tl.tool(toolPath)
-                    .arg(pulCommand);
-        const pulArgs = tl.getDelimitedInput("args", " ");
-        pulArgs.forEach((arg: string) => {
-            pulCommandRunner.arg(arg);
-        });
-        exitCode = await pulCommandRunner.exec(pulExecOptions);
-        if (exitCode !== 0) {
-            tl.setResult(tl.TaskResult.Failed,
-                tl.loc("PulumiCommandFailed", exitCode, `${ pulCommand } ${ pulArgs.join(" ") }`));
-            return;
-        }
-    } catch (err) {
-        tl.setResult(tl.TaskResult.Failed, err);
-    }
+    tl.debug(tl.loc("Debug_Installed"));
+    await runPulumi(serviceEndpoint);
 }
 
 async function installPulumi(): Promise<number> {
