f6d4e4a6d8b7d81229c8d2d3b64987d0d1e849b8
David O'Brien <me@david-obrien.net>
2019-05-14 12:22:56 +1000

    Add windows support

--- buildAndReleaseTask/installers/pulumi.ts
+++ buildAndReleaseTask/installers/pulumi.ts
@@ -9,22 +9,34 @@ export async function installPulumiWithToolLib(expectedVersion: string, latestPu
   let downloadUrl: string;
   const os: string = tl.osType();
   tl.debug(tl.loc("OSDETECTED", os));
-
   if (expectedVersion.toLowerCase() === "latest") {
     expectedVersion = latestPulumiVersion;
   }
-
   tl.debug("Pulumi version to install is " + expectedVersion);
 
-  try {
-    // tslint:disable-next-line:max-line-length
-    downloadUrl = "https://get.pulumi.com/releases/sdk/pulumi-v" + expectedVersion + "-" + os.toLowerCase() + "-x64.tar.gz";
-    const temp: string = await lib.downloadTool(downloadUrl);
-    const extractTemp: string = await lib.extractTar(temp);
-    lib.prependPath(path.join(extractTemp, "pulumi"));
-    tl.debug(tl.loc("Debug_Installed"));
-    tl.debug(tl.loc("Debug_AddedToPATH"));
-  } catch (err) {
-    tl.setResult(tl.TaskResult.Failed, tl.loc("PulumiInstallFailed", err.message));
+  if (os.toLowerCase() === "windows_nt") {
+    try {
+      downloadUrl = "https://get.pulumi.com/releases/sdk/pulumi-v" + expectedVersion + "-windows-x64.zip";
+      const temp: string = await lib.downloadTool(downloadUrl);
+      const extractTemp: string = await lib.extractZip(temp);
+      lib.prependPath(path.join(extractTemp, "pulumi"));
+      tl.debug(tl.loc("Debug_Installed"));
+      tl.debug(tl.loc("Debug_AddedToPATH"));
+    } catch (err) {
+      tl.setResult(tl.TaskResult.Failed, tl.loc("PulumiInstallFailed", err.message));
+    }
+  }
+  if (os.toLowerCase() === "linux") {
+    try {
+      // tslint:disable-next-line:max-line-length
+      downloadUrl = "https://get.pulumi.com/releases/sdk/pulumi-v" + expectedVersion + "-" + os.toLowerCase() + "-x64.tar.gz";
+      const temp: string = await lib.downloadTool(downloadUrl);
+      const extractTemp: string = await lib.extractTar(temp);
+      lib.prependPath(path.join(extractTemp, "pulumi"));
+      tl.debug(tl.loc("Debug_Installed"));
+      tl.debug(tl.loc("Debug_AddedToPATH"));
+    } catch (err) {
+      tl.setResult(tl.TaskResult.Failed, tl.loc("PulumiInstallFailed", err.message));
+    }
   }
 }
