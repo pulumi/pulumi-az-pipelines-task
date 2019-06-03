37d7382f9d4871bd0fafa242c9af539f3c0c9dd1
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2019-06-03 10:23:04 -0700

    Oops. Missed committing newly added file.

---
+++ buildAndReleaseTask/vars.ts
@@ -0,0 +1,19 @@
+// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+
+/**
+ * This file contains the various environment variables set by the task extension.
+ */
+
+// INSTALLED_PULUMI_VERSION is set after a version of the CLI has been installed successfully.
+export const INSTALLED_PULUMI_VERSION = "INSTALLED_PULUMI_VERSION";
+
+// PULUMI_ACCESS_TOKEN is used by the Pulumi CLI to login into an account non-interactively.
+export const PULUMI_ACCESS_TOKEN = "PULUMI_ACCESS_TOKEN";
+/**
+ * PULUMI_CONFIG_PASSPHRASE is used to specify the passphrase used by the secrets provider
+ * to be able to decrypt a secret that was previously encrypted by deriving a key
+ * from the same passphrase.
+ *
+ * See https://blog.pulumi.com/managing-secrets-with-pulumi to learn more about encrypted configuration properties.
+ */
+export const PULUMI_CONFIG_PASSPHRASE = "PULUMI_CONFIG_PASSPHRASE";
