764f4c08ce01a4aeae81c95429f267b6c78095e7
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2020-10-28 12:30:29 -0400

    Fix for using Azure Storage backend with env vars and Azure KeyVault secrets provider (#51)
    * Update packages
    
    * Set the process.env vars in the execution environment of the login command as well. Set AZURE_* env vars from the Service Connection.
    
    * Update test npm packages.
    
    * Use strictEqual since assert.equal is deprecated.

--- buildAndReleaseTask/tests/_suite.ts
+++ buildAndReleaseTask/tests/_suite.ts
@@ -5,14 +5,14 @@ import * as ttm from "azure-pipelines-task-lib/mock-test";
 import * as path from "path";
 
 function mustNotHaveErrorsOrWarnings(tr: ttm.MockTestRunner) {
-    assert.equal(tr.succeeded, true, "should have succeeded");
-    assert.equal(tr.warningIssues.length, 0, "should have no warnings");
-    assert.equal(tr.errorIssues.length, 0, "should have no errors");
+    assert.strictEqual(tr.succeeded, true, "should have succeeded");
+    assert.strictEqual(tr.warningIssues.length, 0, "should have no warnings");
+    assert.strictEqual(tr.errorIssues.length, 0, "should have no errors");
 }
 
 describe("Pulumi task tests", () => {
 
-    it("should install the CLI and run command", (done: MochaDone) => {
+    it("should install the CLI and run command", (done: Mocha.Done) => {
         const tp = path.join(__dirname, "success.js");
         const tr = new ttm.MockTestRunner(tp);
 
@@ -24,15 +24,15 @@ describe("Pulumi task tests", () => {
         // for some odd reason, doing so is causing the mock-test library to print debugging
         // output always, even when not setting the `TASK_TEST_TRACE` env var before running the test.
         const expectedVersion = "0.16.5";
-        assert.equal(stdout.indexOf(expectedVersion) >= 0, true, "should execute `pulumi version` command");
-        assert.equal(stdout.indexOf("stack selected") >= 0, true, "should select stack");
-        assert.equal(stdout.indexOf("fake logged in") >= 0, true, "should login");
-        assert.equal(stdout.indexOf("fake pulumi preview") >= 0, true, "should execute `pulumi preview` command");
+        assert.strictEqual(stdout.indexOf(expectedVersion) >= 0, true, "should execute `pulumi version` command");
+        assert.strictEqual(stdout.indexOf("stack selected") >= 0, true, "should select stack");
+        assert.strictEqual(stdout.indexOf("fake logged in") >= 0, true, "should login");
+        assert.strictEqual(stdout.indexOf("fake pulumi preview") >= 0, true, "should execute `pulumi preview` command");
 
         done();
     });
 
-    it("should run Pulumi with the expected env vars", (done: MochaDone) => {
+    it("should run Pulumi with the expected env vars", (done: Mocha.Done) => {
         const tp = path.join(__dirname, "envvars.js");
         const tr = new ttm.MockTestRunner(tp);
 
@@ -42,13 +42,13 @@ describe("Pulumi task tests", () => {
         done();
     });
 
-    it("should run Pulumi with Azure Storage with the expected env vars", (done: MochaDone) => {
+    it("should run Pulumi with Azure Storage with the expected env vars", (done: Mocha.Done) => {
         const tp = path.join(__dirname, "envvars_storage.js");
         const tr = new ttm.MockTestRunner(tp);
 
         tr.run();
         mustNotHaveErrorsOrWarnings(tr);
-        assert.equal(
+        assert.strictEqual(
             tr.stdout.indexOf("fake logged in using azure storage") >= 0,
             true,
             "should login using azure environment variables");
@@ -56,13 +56,13 @@ describe("Pulumi task tests", () => {
         done();
     });
 
-    it("should create the stack if it does not exist", (done: MochaDone) => {
+    it("should create the stack if it does not exist", (done: Mocha.Done) => {
         const tp = path.join(__dirname, "create_stack_if_not_found.js");
         const tr = new ttm.MockTestRunner(tp);
 
         tr.run();
         mustNotHaveErrorsOrWarnings(tr);
-        assert.equal(tr.stdout.indexOf("Created stack") >= 0, true, "should create stack");
+        assert.strictEqual(tr.stdout.indexOf("Created stack") >= 0, true, "should create stack");
 
         done();
     });
