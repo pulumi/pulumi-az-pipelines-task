5d3fef140d58f2004ecd70887cce7b92303f8c4d
Zach Buchheit <zbchht@gmail.com>
2023-12-04 10:52:43 -0800

    Update copyright years in included copyright
    Co-authored-by: Justin Van Patten <jvp@justinvp.com>

--- buildAndReleaseTask/awsServiceEndpoint.ts
+++ buildAndReleaseTask/awsServiceEndpoint.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2023, Pulumi Corporation.  All rights reserved.
 
 import * as tl from "azure-pipelines-task-lib/task";
 
